33757922503b40351284f1d46e012d235618a83e
Khoi Nguyen <khoi@gkxim.com>
2017-04-05 12:05:58 +0700

    initial commit for react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return ( 
+          <div style={{ textAlign: 'center'}}>
+            <h1> Sơn bùi Landing page </h1>
+          </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+var path = require('path');
+
+module.exports = {
+  entry: __dirname + '/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-0']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
