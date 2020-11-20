55d2ab6be1e8623936fc27379d134cb1a2c7c10b
sonbui <sonbui.gfd@gmail.com>
2020-11-20 09:10:00 +0700

    update intro page

--- src/Reducers.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import home from './pages/home/reducer';
-
-export default combineReducers({
-  home,
-});
--- src/Store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import Reducers from './Reducers';
-
-const store = createStore(Reducers, applyMiddleware(ReduxThunk));
-
-export default store;
